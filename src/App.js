4ac3be9474321517cc7ea331dead31d3261a3c31
suneel53 <suneelkandula53@gmail.com>
2023-10-13 02:42:48 +0000

    Solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Speedometer from './components/Speedometer'
+
+import './App.css'
+
+const App = () => (
+  <div className="bg-cont">
+    <Speedometer imgUrl="https://assets.ccbp.in/frontend/react-js/speedometer-img.png" />
+  </div>
+)
+
+export default App
---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,45 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class Speedometer extends Component {
+  state = {count: 0}
+
+  onacc = () => {
+    this.setState(prevState => {
+      console.log('acceleration by 10')
+      return {count: prevState.count + 10}
+    })
+  }
+
+  onbrake = () => {
+    this.setState(prevState => {
+      console.log('brake applied')
+      return {count: prevState.count - 10}
+    })
+  }
+
+  render() {
+    const {imgUrl} = this.props
+    const {count} = this.state
+    return (
+      <div className="cont">
+        <h1>Speedometer</h1>
+        <img src={imgUrl} alt="speedometer" />
+        <h1>Speed is {count}mph</h1>
+        <p>Min Limit is 0mph, Max Limit is 200mph</p>
+        <div>
+          <button type="button" className="but1" onClick={this.onacc}>
+            Accelerate
+          </button>
+          <button type="button" className="but2" onClick={this.onbrake}>
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
